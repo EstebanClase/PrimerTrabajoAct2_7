499b6efb64d7431496bcb4bf5a6dd1602f7b2ec1
EstebanClase <esteeban.e@gmail.com>
2020-09-28 17:34:33 -0300

    trabajo

--- app/src/main/java/com/example/primertrabajo/MainActivity.java
+++ app/src/main/java/com/example/primertrabajo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.primertrabajo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Ingresar(View view){
+        Intent ingresar = new Intent(this, MainActivity3.class);
+        startActivity(ingresar);
+    }
+
+    public void Registrar(View view){
+        Intent registrar = new Intent(this, MainActivity2.class);
+        startActivity(registrar);
     }
 }
---
+++ app/src/main/java/com/example/primertrabajo/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.primertrabajo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void Cerrar(View view){
+        finish();
+    }
+}
